2b55554bd9fd5629ec85499317a2b978aa0ba6d2
Justin Santos <83016833+Ozzie-code-alt@users.noreply.github.com>
2024-12-23 08:16:12 +0800

    chore: setup firebase config

--- dev-connect/lib/firebaseConfig.js
+++ dev-connect/lib/firebaseConfig.js
@@ -1,23 +1,30 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+import { getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAvz_73YGl3ppOo9apzb5My9pdtDa-SVJk",
-  authDomain: "fir-social-media-platform.firebaseapp.com",
-  projectId: "fir-social-media-platform",
-  storageBucket: "fir-social-media-platform.firebasestorage.app",
-  messagingSenderId: "812661427722",
-  appId: "1:812661427722:web:2854228abc2f5755d319f3",
-  measurementId: "G-6S0ZCBN47D"
+  apiKey: 'AIzaSyAvz_73YGl3ppOo9apzb5My9pdtDa-SVJk',
+  authDomain: 'fir-social-media-platform.firebaseapp.com',
+  projectId: 'fir-social-media-platform',
+  storageBucket: 'fir-social-media-platform.firebasestorage.app',
+  messagingSenderId: '812661427722',
+  appId: '1:812661427722:web:2854228abc2f5755d319f3',
+  measurementId: 'G-6S0ZCBN47D'
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export {app, analytics}
+
+
+export { app, provider, auth };
